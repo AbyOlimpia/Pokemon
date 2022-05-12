9058c546803f8611d7b7cb650fbfa3a1da7bacbc
Alejandro Moreno <alejandro.s258153@cesurformacion.com>
2022-05-12 20:02:03 +0200

    MySQLconnector

---
+++ src/MySQLconnector.java
@@ -0,0 +1,39 @@
+package src;
+
+import java.io.IOException;
+import java.sql.*;
+import javax.sql.*;
+
+public class MySQLconnector {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/pokemon";
+        String login = "root";
+        String password = "";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, login, password);
+
+            System.out.println("Conexión establecida con éxito");
+
+            /**
+             * A partir de esta línea la conexión queda establecida, por lo que se puede hacer cualquier insert, select etc...
+             */
+            try {
+                connection.close();
+                System.out.println("Conexión cerrada");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
