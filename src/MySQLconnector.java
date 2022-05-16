7d3cc1439f643608f80810714ea574db0cacda17
AlexCesur <alejandro.s258153@cesurformacion.com>
2022-05-16 13:12:08 +0200

    Nueva rama, pruebas para SQL

--- src/MySQLconnector.java
+++ src/MySQLconnector.java
@@ -36,3 +36,48 @@ public class MySQLconnector {
         }
     }
 }
+
+
+public static void mostrarPokemon (Connection con) throws SQLException {
+    String consulta = "SELECT * FROM POKEDEX";
+    Statement statement = com.createStatement();
+    ResultSet rs = statement.executeQuery(consulta);
+
+
+    Pokemon e = null;
+    while (rs.next()) {
+        e = new Pokemon();
+        e.setNumPokedex(rs.getInt("num_pokedex"));
+        e.setNomPokemon(rs.getString("nom_pokemon"));
+        e.tipo(rs.getString("tipo"));
+
+        System.out.println(e.toString());
+        
+    }
+    statement.close();
+
+}
+
+
+public static void insertarPokemon(Connection con, Pokemon p) throws SQLException {
+    String sentencia ="INSERT INTO POKEDEX(NUM_POKEDEX,NOM_POKEMON, TIPO) VALUES("+ p.getnum_Pokedex()
+                                                   +",'"+p.getnom_Pokemon()
+                                                   +"','"+p.gettipo()
+                                                   +"')";
+    Statement stmt = null;
+    try {
+        stmt = con.createStatement();
+        stmt.executeUpdate(sentencia);
+        
+        System.out.println("Nuevo pokemon insertado. "+p.getNomPokemon());
+    } catch (SQLException e) {
+        e.printStackTrace();
+    } finally {
+        stmt.close(); // Puede lanzar SQLExceptions, por eso he puesto el
+        // throws en la cabecera del método
+    }
+                                                   
+}
+
+
+}
