4a5af489eb21b821c1ab9c4ba501b0f44204a5ac
Olimpia <gallardo.olimpia.2000@gmail.com>
2022-05-19 13:42:45 +0200

    Entrenamiento pesado y ligero made by (@AlexCesur)

--- src/entrenador/Entrenador.java
+++ src/entrenador/Entrenador.java
@@ -1,11 +1,13 @@
 package src.entrenador;
 
+import src.pokemon.Pokemon;
+
 public class Entrenador {
     private String nombre;
     private int pokedollar;
 
     Entrenador(String paramNombre) {
-        this.nombre=paramNombre;
+        this.nombre = paramNombre;
 
     }
 
@@ -17,5 +19,28 @@ public class Entrenador {
         return pokedollar;
     }
 
-    //prueba2
+    /**
+     * Con este método mejoramos la defensa, la defensa especial y la vitalidad
+     * Con un coste de (20 * el nivel del pokemon a mejorar) Pokedóllares
+     * @param pokemon
+     */
+    public void entrenamientoPesado(Pokemon pokemon) {
+        pokedollar = pokedollar - 20 * pokemon.getNivel();
+        pokemon.setDefensaSp(pokemon.getDefensaSp() + 5);
+        pokemon.setDefensa(pokemon.getDefensa() + 5);
+        pokemon.setVitalidad(pokemon.getVitalidad() + 5);
+    }
+
+    /**
+     * Con este método mejoramos el ataque, el ataque especial y la vitalidad 
+     * Con un coste de (30 * el nivel del pokemon a mejorar) Pokedóllares
+     * @param pokemon
+     */
+    public void entrenamientoFurioso(Pokemon pokemon) {
+        pokedollar = pokedollar - 30 * pokemon.getNivel();
+        pokemon.setAtaque(pokemon.getAtaque() + 5);
+        pokemon.setAtaqueSp(pokemon.getAtaqueSp() + 5);
+        pokemon.setVitalidad(pokemon.getVitalidad() + 5);
+    }
+
 }
